80679b561cbe6116ebb3c916cc7edae3fc8ef766
Shaikh Saqlain <saqlainsk18@gmail.com>
2022-05-19 23:04:42 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,52 @@
+abc = 10
+while(abc<20):
+    if(abc == 14):
+        continue
+    if(abc == 12):
+        break
+    abc = abc + 1
+    print(abc)
+    
+  
+  # 2
+  
+  while(abc < 20):
+    abc = abc + 1
+    print(abc)
+    if(abc == 13):
+        continue
+    if(abc == 15):
+        break
+        
+output:
+11
+12
+13
+14
+15
+
+# 3
+
+while(abc < 20):
+    abc = abc + 1
+    print(abc)
+    if(abc == 13):
+        continue
+    if(abc == 15):
+        break
+        
+output : 
+      
+11
+12
+13
+14
+15
+
+# 4
+
+
+
+
+  
+  
